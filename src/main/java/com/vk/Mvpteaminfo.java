d16ed0e7d1d8e52c584e9837a709f3c3a2b1c6a8
vgkadam <vishal.kadam@oracle.com>
2022-04-01 13:48:31 +0530

    Modifying

--- src/main/java/com/vk/Mvpteaminfo.java
+++ src/main/java/com/vk/Mvpteaminfo.java
@@ -1,39 +1,47 @@
 package com.vk;
 
 import javax.persistence.Entity;
+
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 @Entity
 @Table(name = "mvpteaminfo")
-
 public class Mvpteaminfo {
-	
+
 	@Id
-    private Long mvp_team_id;
-    private String mvp_team_member;
-    private Long mvpid;
+	private Long mvp_team_id;
+	private String mvp_team_member;
+	private String mvpname;
+
 	public Long getMvp_team_id() {
 		return mvp_team_id;
 	}
+
 	public void setMvp_team_id(Long mvp_team_id) {
 		this.mvp_team_id = mvp_team_id;
 	}
+
 	public String getMvp_team_member() {
 		return mvp_team_member;
 	}
+
 	public void setMvp_team_member(String mvp_team_member) {
 		this.mvp_team_member = mvp_team_member;
 	}
-	public Long getMvpid() {
-		return mvpid;
+
+	public String getMvpid() {
+		return mvpname;
 	}
-	public void setMvpid(Long mvpid) {
-		this.mvpid = mvpid;
+
+	public void setMvpid(String mvpname) {
+		this.mvpname = mvpname;
 	}
-	public Mvpteaminfo(Long mvp_team_id, String mvp_team_member, Long mvpid) {
+
+	public Mvpteaminfo(Long mvp_team_id, String mvp_team_member, String mvpname) {
 		super();
 		this.mvp_team_id = mvp_team_id;
 		this.mvp_team_member = mvp_team_member;
-		this.mvpid = mvpid;
+		this.mvpname = mvpname;
 	}
-   }
+}
